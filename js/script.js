0a7d0c107bd73305f818522e1175be7ed8ff5e89
Justin Goodchild <jeg6300@gmail.com>
2020-12-16 01:38:39 -0500

    script.js now properly adds the 4 different consoles. Modified index.html using code from project 2 to include the cards from the catalogue page so as to look pretty-ish

--- js/script.js
+++ js/script.js
@@ -2,16 +2,24 @@ jQuery(document).ready(function($){
     const request = axios.get('http://csc225.mockable.io/consoles');
     request.then(function(response){
         $('#loading').hide();
-        console.log("response get");
         const platforms = response.data;
-        /*const platformsHtml = platforms.map(function(platform){
-            const {id, name: consoleName, image} = platform;
+        const platformsHtml = platforms.map(function(platform){
+            const {id, name: platformName, image} = platform;
             return `
-                <div>
-                    <p class="m-0"
+                <div class="col-lg-4 d-flex align-items-stretch">
+                    <div class="card mb-4 shadow-sm" data-id=${id}>
+                        <img src="${image}" class="img-fluid" alt="Responsive image">
+                        <div class="card-body">
+                            <h3><i class="text-muted">${platformName}</i></h3>
+                            <p class="card-text"></p>
+                            <a class="btn btn-primary">More Information</a>
+                        </div>
+                    </div>
                 </div>
             `;
-        }).join('')*/
+        }).join('');
+    $('#platforms').html(platformsHtml);
     });
-    //$('#platforms').html(platformsHtml);
 });
+
+    
