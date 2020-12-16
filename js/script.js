f8e235692f1e733ace7bc708d78ed1783e47bf51
Justin Goodchild <jeg6300@gmail.com>
2020-12-16 02:04:43 -0500

    Finished the javascript. Made it so clicking a button appends the information to the proper card. Validated html

--- js/script.js
+++ js/script.js
@@ -7,18 +7,32 @@ jQuery(document).ready(function($){
             const {id, name: platformName, image} = platform;
             return `
                 <div class="col-lg-4 d-flex align-items-stretch">
-                    <div class="card mb-4 shadow-sm" data-id=${id}>
+                    <div class="card mb-4 shadow-sm">
                         <img src="${image}" class="img-fluid" alt="Responsive image">
                         <div class="card-body">
                             <h3><i class="text-muted">${platformName}</i></h3>
-                            <p class="card-text"></p>
-                            <a class="btn btn-primary">More Information</a>
+                            <p class="card-text" id="card-id-${id}"></p>
+                            <a class="btn btn-primary" data-id="${id}">More Information</a>
                         </div>
                     </div>
                 </div>
             `;
         }).join('');
     $('#platforms').html(platformsHtml);
+    });
+
+    jQuery('#platforms').on('click','.btn',function(){
+        const id = $(this).attr('data-id');
+        $('#loading').show();
+        axios.get(`http://csc225.mockable.io/consoles/${id}`).then(function(info){
+            $('#loading').hide();
+            const{price,country,releaseYear} = info.data;
+            $('#card-id-'+id).html(`
+                Price of Console: $${price}<br>
+                Country: ${country}<br>
+                Release Year: ${releaseYear}<br>
+            `);
+        });
     });
 });
 
