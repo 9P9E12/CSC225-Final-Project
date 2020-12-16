3ffb37192de1070155d44904447e6ca0a765bd79
Justin Goodchild <jeg6300@gmail.com>
2020-12-16 00:51:17 -0500

    Added initial loading screen while site gets the data for the consoles that disappears once a response is gained

--- js/script.js
+++ js/script.js
@@ -1,13 +1,17 @@
 jQuery(document).ready(function($){
     const request = axios.get('http://csc225.mockable.io/consoles');
     request.then(function(response){
-        const consoles = response.data;
-        const consolesHtml = consoles.map(function(console){
-            const {id, name: consoleName, image} = console;
+        $('#loading').hide();
+        console.log("response get");
+        const platforms = response.data;
+        /*const platformsHtml = platforms.map(function(platform){
+            const {id, name: consoleName, image} = platform;
             return `
-                
+                <div>
+                    <p class="m-0"
+                </div>
             `;
-        }).join('')
+        }).join('')*/
     });
-    console.log('after the request');
+    //$('#platforms').html(platformsHtml);
 });
